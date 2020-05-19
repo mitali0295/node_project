d0d7ef63c2fd060c81e9df0ec7c032e9f62c2fa4
Mitali Manjarekar <manjarekar.m@northeastern.edu>
2020-05-19 15:57:14 -0400

    added sequelize

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,4 +1,10 @@
 var get_landing = function(req, res, next) {
   res.render('landing', { title: 'Express' });
 };
+var submit_lead = function(req,res,next) {
+	console.log("lead email:",req.body.lead_email);
+	res.redirect('/');
+};
+
 exports.get_landing = get_landing;
+exports.submit_lead = submit_lead;
